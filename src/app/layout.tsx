28a88e9bda952750c5f9bafc1c4896e7ee684fb1
Wandy R. Santana <sntwandy@gmail.com>
2024-01-30 10:24:04 -0400

    feat: Add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
       <body className={inter.className}>
         {children}
         <span className='developed-by'>Developed by: @sntwandy</span>
